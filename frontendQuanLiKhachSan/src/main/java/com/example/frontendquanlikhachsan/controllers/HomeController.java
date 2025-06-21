806275c2139b821168a7a87bf3b88d8e2dc2a269
nguyenminhquang <nnguyenminhquang786@gmail.com>
2025-06-21 17:27:52 +0700

    update navbar and home page

--- frontendQuanLiKhachSan/src/main/java/com/example/frontendquanlikhachsan/controllers/HomeController.java
+++ frontendQuanLiKhachSan/src/main/java/com/example/frontendquanlikhachsan/controllers/HomeController.java
@@ -3,6 +3,7 @@ package com.example.frontendquanlikhachsan.controllers;
 import com.example.frontendquanlikhachsan.ApiHttpClientCaller;
 import com.example.frontendquanlikhachsan.auth.TokenHolder;
 import com.example.frontendquanlikhachsan.entity.account.ResponseAccountDto;
+import com.example.frontendquanlikhachsan.entity.enums.Sex;
 import com.example.frontendquanlikhachsan.entity.staff.ResponseStaffDto;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
@@ -10,9 +11,12 @@ import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.util.Duration;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
@@ -21,7 +25,19 @@ public class HomeController {
     @FXML
     private Label helloLabel, clockLabel;
     @FXML
-    private Label staffIdLabel, staffNameLabel, staffEmailLabel, staffRoleLabel;
+    private Label staffIdLabel,
+            staffNameLabel,
+            staffEmailLabel,
+            staffRoleLabel,
+            staffIdentifyNumberLabel,
+            staffAddressLabel,
+            staffAgeLabel,
+            staffPositionLabel;
+
+    @FXML
+    private Label dateDayLabel;
+    @FXML
+    private ImageView avatarImageView;
 
     @FXML
     public void initialize() {
@@ -32,7 +48,22 @@ public class HomeController {
     private void startClock() {
         Timeline clock = new Timeline(new KeyFrame(Duration.seconds(1), e -> {
             LocalTime currentTime = LocalTime.now();
-            clockLabel.setText(currentTime.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+            LocalDate currentDate = LocalDate.now();
+            DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+            String dayOfWeek = switch (currentDate.getDayOfWeek()) {
+                case MONDAY -> "Thứ hai";
+                case TUESDAY -> "Thứ ba";
+                case WEDNESDAY -> "Thứ tư";
+                case THURSDAY -> "Thứ năm";
+                case FRIDAY -> "Thứ sáu";
+                case SATURDAY -> "Thứ bảy";
+                case SUNDAY -> "Chủ nhật";
+            };
+
+            clockLabel.setText(currentTime.format(timeFormatter));
+            dateDayLabel.setText(currentDate.format(dateFormatter) + " – " + dayOfWeek);
         }));
         clock.setCycleCount(Animation.INDEFINITE);
         clock.play();
@@ -45,6 +76,15 @@ public class HomeController {
                 String staffJson = ApiHttpClientCaller.call("staff/" + currentUserId, ApiHttpClientCaller.Method.GET, null);
                 ResponseStaffDto staffDto = ApiHttpClientCaller.mapper.readValue(staffJson, ResponseStaffDto.class);
 
+                if (staffDto.getSex()== Sex.MALE) {
+                    Image avatar = new Image(getClass().getResourceAsStream("/com/example/frontendquanlikhachsan/assets/images/male_staff_icon.png"));
+                    avatarImageView.setImage(avatar);
+                }
+                else {
+                    Image avatar = new Image(getClass().getResourceAsStream("/com/example/frontendquanlikhachsan/assets/images/female_staff_icon.png"));
+                    avatarImageView.setImage(avatar);
+                }
+
                 String accountJson = ApiHttpClientCaller.call("account/" + staffDto.getAccountId(), ApiHttpClientCaller.Method.GET, null);
                 ResponseAccountDto accountDto = ApiHttpClientCaller.mapper.readValue(accountJson, ResponseAccountDto.class);
 
@@ -54,8 +94,12 @@ public class HomeController {
                     helloLabel.setText("Xin chào, " + role + " " + staffDto.getFullName());
                     staffIdLabel.setText(String.valueOf(staffDto.getId()));
                     staffNameLabel.setText(staffDto.getFullName());
-                    staffEmailLabel.setText(staffDto.getIdentificationNumber());
+                    staffEmailLabel.setText(staffDto.getEmail());
+                    staffIdentifyNumberLabel.setText(staffDto.getIdentificationNumber());
                     staffRoleLabel.setText(role);
+                    staffAddressLabel.setText(staffDto.getAddress());
+                    staffAgeLabel.setText(String.valueOf(staffDto.getAge()));
+                    staffPositionLabel.setText(staffDto.getPositionName());
                 });
 
             } catch (Exception e) {
@@ -63,5 +107,4 @@ public class HomeController {
             }
         }).start();
     }
-
 }
